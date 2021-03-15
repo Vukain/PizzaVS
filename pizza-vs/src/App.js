c9b093d58186da776d7eaeef6e72e4ab3a98351a
Michal Piechowiak <nilfenheim@gmail.com>
2021-03-15 15:39:16 +0100

    add pizza img

--- pizza-vs/src/App.js
+++ pizza-vs/src/App.js
@@ -2,14 +2,16 @@ import './App.sass';
 import PizzaInput from './components/PizzaInput/PizzaInput.jsx';
 import ShowResult from './components/ShowResult/ShowResult.jsx';
 import AppProvider from './AppContext'
+import Pizza from './components/Pizza/Pizza';
 
 function App() {
   return (
     <AppProvider>
       <div className="App">
-        <PizzaInput name='uno' />
+        {/* <PizzaInput name='uno' />
         <PizzaInput name='due' />
-        <ShowResult />
+        <ShowResult /> */}
+        <Pizza />
       </div>
     </AppProvider>
   );
---
+++ pizza-vs/src/components/Pizza/Pizza.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Pizza.sass';
+import { ReactComponent as PizzaImg } from '../../media/pizza.svg';
+
+const Pizza = () => {
+    return (
+        <div className='rucola'>
+            <PizzaImg />
+        </div>
+    );
+}
+
+export default Pizza;

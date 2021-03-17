fbccf1b1bc3c289190574c520780f7fb3424030c
Michal Piechowiak <nilfenheim@gmail.com>
2021-03-17 18:12:04 +0100

    change: store structure

--- pizza-vs/src/components/PizzaInput/PizzaInput.jsx
+++ pizza-vs/src/components/PizzaInput/PizzaInput.jsx
@@ -1,36 +1,24 @@
 import React, { useContext, useEffect, useState } from 'react';
+
 import './PizzaInput.sass';
 import { AppContext } from '../../AppContext';
 
-
 const PizzaInput = (props) => {
 
-    const { pizzaUnoData, setPizzaUnoData } = useContext(AppContext);
-    const { pizzaDueData, setPizzaDueData } = useContext(AppContext);
+    const pizzaData = useContext(AppContext)
+    const { getPizzaData, setPizzaData } = pizzaData[props.name];
 
-    let pizzaData = {};
-
-    if (props.name === 'uno') {
-        pizzaData = pizzaUnoData;
-    } else {
-        pizzaData = pizzaDueData;
-    }
-
-    const [pizzaSize, setPizzaSize] = useState(pizzaData.size);
-    const [pizzaCount, setPizzaCount] = useState(pizzaData.count);
-    const [pizzaPrice, setPizzaPrice] = useState(pizzaData.price);
+    const [pizzaSize, setPizzaSize] = useState(getPizzaData.size);
+    const [pizzaCount, setPizzaCount] = useState(getPizzaData.count);
+    const [pizzaPrice, setPizzaPrice] = useState(getPizzaData.price);
 
     useEffect(() => {
         let surface = 3.14 * ((pizzaSize / 2) ** 2) * pizzaCount;
         let value = surface / pizzaPrice;
 
-        if (props.name === 'uno') {
-            setPizzaUnoData({ size: pizzaSize, count: pizzaCount, price: pizzaPrice, surface: surface, value: value });
-        } else {
-            setPizzaDueData({ size: pizzaSize, count: pizzaCount, price: pizzaPrice, surface: surface, value: value });
-        }
-
-    }, [pizzaPrice, pizzaCount, pizzaSize])
+        setPizzaData({ size: pizzaSize, count: pizzaCount, price: pizzaPrice, value: value });
+    }
+        , [pizzaPrice, pizzaCount, pizzaSize])
 
     return (
         <div className={props.classer}>
@@ -40,7 +28,8 @@ const PizzaInput = (props) => {
                 <label htmlFor="">Ilość: <input type="number" onChange={({ target }) => { setPizzaCount(parseInt(target.value)) }} value={pizzaCount} /></label>
                 <label htmlFor="">Łączna cena: <input type="number" onChange={({ target }) => { setPizzaPrice(parseInt(target.value)) }} /></label>
             </form>
-            <button onClick={() => { console.log(pizzaData) }}>Loguj</button>
+            {/* <button onClick={() => { console.log(pizzaData) }}>Loguj</button> */}
+            <p>Wartośc: {getPizzaData.value}</p>
         </div>);
 }
 
